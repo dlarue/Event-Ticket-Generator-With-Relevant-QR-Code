0638fc5ee0fbeccdf528568999a263fe47e755a6
Doug LaRue <doug.larue@gmail.com>
2021-05-21 17:09:40 -0700

    fixing ZXing lib issues

---
+++ buildAll.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd lib
+wget https://repo1.maven.org/maven2/com/google/zxing/javase/3.4.1/javase-3.4.1.jar
+wget https://repo1.maven.org/maven2/com/google/zxing/core/3.4.1/core-3.4.1.jar
+cd ..
+mvn clean
+mvn compile
+mvn install:install-file -Dfile=./lib/core-3.4.1.jar \
+    -DgroupId=com.google.zxing \
+    -DartifactId=core-3.4.1 \
+    -Dversion=3.4.1 \
+    -Dpackaging=jar \
+    -DlocalRepositoryPath=./lib \
+#mvn install:install-file -Dfile=./lib/javase-3.4.1.jar \
+     -Dfile=./lib/javase-3.4.1.jar \
+    -DgroupId=com.google.zxing \
+    -DartifactId=javase-3.4.1 \
+    -Dversion=3.4.1 \
+    -Dpackaging=jar \
+    -DlocalRepositoryPath=./lib \
+    package
+#mvn package
+
+echo "java -classpath ./lib/core-3.4.1.jar:./lib/javase-3.4.1.jar:./QRGenerator.0-SNAPSHOT.jar userInterface"
